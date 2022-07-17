ee3f4f3df77f36ad28211d512f0dac5a26a03d96
unleftie <23164223+unleftie@users.noreply.github.com>
2022-07-17 18:02:40 +0300

    start automatically after a system reboot

---
+++ files/cron.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Wireguard interface doesn`t start automatically after a system reboot
+
+while getopts i: option; do
+    case "${option}" in
+    i) INTERFACE=${OPTARG} ;;
+    esac
+done
+
+if [ -z "$INTERFACE" ]; then
+    echo "Not enough arguments [-i INTERFACE]"
+    exit 1
+fi
+
+if [ ! -f "/etc/wireguard/$INTERFACE.conf" ]; then
+    echo "Wireguard interface config does not exist"
+    exit 1
+fi
+
+while ! wg show $INTERFACE >/dev/null 2>&1; do
+    wg-quick up $INTERFACE && break
+    sleep 10
+    echo "Wireguard interface /etc/wireguard/$INTERFACE.conf is down. Trying again..."
+done
+
+echo "Wireguard interface is up and running"
+
+exit 0
