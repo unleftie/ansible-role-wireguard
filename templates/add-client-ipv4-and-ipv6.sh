cbb9bc6fc8b7104111ecc96924c928e1c1c794a2
denysrondaliev <denysrondaliev@gmail.com>
2024-02-18 19:45:13 +0200

    fix: script to add new clients for ipv6

--- templates/add-client-ipv4-and-ipv6.sh
+++ templates/add-client-ipv4-and-ipv6.sh
@@ -16,21 +16,23 @@ function print_success() {
     printf '%s# %s%s\n' "$(printf '\033[32m')" "$*" "$(printf '\033[m')" >&2
 }
 
+function print_warning() {
+    printf '%sWARNING: %s%s\n' "$(printf '\033[31m')" "$*" "$(printf '\033[m')" >&2
+}
+
 function print_error() {
     printf '%sERROR: %s%s\n' "$(printf '\033[31m')" "$*" "$(printf '\033[m')" >&2
     exit 1
 }
 
 function get_input() {
-    [[ $ZSH_VERSION ]] && read "$2"\?"$1"
-    [[ $BASH_VERSION ]] && read -p "$1" "$2"
+    read -rp "$1" "$2"
 }
 
 function get_keypress() {
     local REPLY IFS=
     printf >/dev/tty '%s' "$*"
-    [[ $ZSH_VERSION ]] && read -rk1
-    [[ $BASH_VERSION ]] && read </dev/tty -rn1
+    read </dev/tty -rn1
     printf '%s' "$REPLY"
 }
 
@@ -56,6 +58,8 @@ function check_ipv4() {
     private_ipv4=()
     for addr in $ipv4_addresses; do
         if [[ $addr =~ ^((10|172\.(1[6-9]|2[0-9]|3[0-1])|192\.168)\.) ]]; then
+            # Replace the last octet with 0
+            addr=$(echo $addr | sed 's/\.[0-9]\{1,3\}$/\.0/')
             private_ipv4+=("$addr/24")
         fi
     done
@@ -64,74 +68,51 @@ function check_ipv4() {
     echo "${private_ipv4[@]}"
 }
 
-function check_ipv6() {
-    # Get all IPv6 addresses assigned to the instance
-    ipv6_addresses=$(ip addr show | grep -oE '\b([0-9a-fA-F]{1,4}::?){1,7}[0-9a-fA-F]{1,4}\b')
-
-    # Filter out private IPv6 addresses (RFC 4193)
-    private_ipv6=()
-    for addr in $ipv6_addresses; do
-        if [[ $addr =~ ^[fcd][0-9a-f]{2} ]]; then
-            private_ipv6+=("$addr/96")
-        fi
-    done
-
-    # Return the pool of private IPv6 addresses
-    echo "${private_ipv6[@]}"
-}
-
-function select_ipv4() {
-    echo "Select the pool of IPv4 addresses to which the client will have access:"
+function select_ipv4_pool() {
+    echo "Select the pool of IPv4 addresses to which the client will have access (if EXTERNAL_ACCESS is disabled):"
     select ip in $(check_ipv4); do
         if [[ -n "$ip" ]]; then
-            echo "Selected IPv4 address pool: $ip"
-            echo "$ip" >./selected_ipv4_addresses
+            echo -e "Selected IPv4 address pool: $ip\n"
+            echo "$ip" >$MAIN_DIRECTORY_PATH/allowed_ipv4_pool.txt
+            chown ${WG_USERNAME}:${WG_GROUP} $MAIN_DIRECTORY_PATH/allowed_ipv4_pool.txt
+            chmod 600 $MAIN_DIRECTORY_PATH/allowed_ipv4_pool.txt
             break
         fi
     done
 }
 
-function select_ipv6() {
-    echo "Select the pool of IPv6 addresses to which the client will have access:"
-    select ip in $(check_ipv6); do
-        if [[ -n "$ip" ]]; then
-            echo "Selected IPv6 address pool: $ip"
-            echo "$ip" >./selected_ipv6_addresses
-            break
-        fi
-    done
+function set_ipv6_pool() {
+    # Set the pool of IPv6 addresses to which the client will have access (if EXTERNAL_ACCESS is disabled)
+    echo "fc00::/7" >$MAIN_DIRECTORY_PATH/allowed_ipv6_pool.txt
+    chown ${WG_USERNAME}:${WG_GROUP} $MAIN_DIRECTORY_PATH/allowed_ipv6_pool.txt
+    chmod 600 $MAIN_DIRECTORY_PATH/allowed_ipv6_pool.txt
 }
 
 select_addresses() {
-    # Check if the files already exist
-    if [ -f "./selected_ipv4_addresses" ] && [ -f "./selected_ipv6_addresses" ]; then
-        echo "Selected addresses already exist. Skipping."
+    # check if the files with addresses already exist
+    if [ -f "$MAIN_DIRECTORY_PATH/allowed_ipv4_pool.txt" ] && [ -f "$MAIN_DIRECTORY_PATH/allowed_ipv6_pool.txt" ]; then
         return
     fi
 
-    select_ipv4
-    select_ipv6
+    select_ipv4_pool
+    set_ipv6_pool
 }
 
-# Call the main function
-select_addresses
-
 function configure_variables() {
-    HOSTNAME=$(hostname)
-    # NIC=$(ip -o -4 route show to default | awk '{print $5}' | head -n 1)
     WG_PORT="{{ wg_port }}"
     WG_INTERFACE="{{ wg_interface }}"
     WG_SHORT_IPV4_POOL_PART="{{ wg_short_ipv4_pool_part }}"
     WG_SHORT_IPV6_POOL_PART="{{ wg_short_ipv6_pool_part }}"
     WG_IPV4_CIDR="{{ wg_ipv4_cidr }}"
     WG_IPV6_CIDR="{{ wg_ipv6_cidr }}"
-
-    MAIN_DIRECTORY_PATH="/etc/wireguard/$WG_INTERFACE-files"
+    WG_USERNAME="{{ wg_system_user }}"
+    WG_GROUP="{{ wg_system_group }}"
+    MAIN_DIRECTORY_PATH="/etc/wireguard/{{ wg_interface }}-files"
 
     SERVER_ENDPOINT_IPV4=$(curl -4 -s ident.me)
     SERVER_ENDPOINT_IPV6=$(curl -6 -s ident.me)
     SERVER_PUB_KEY_PATH="$MAIN_DIRECTORY_PATH/server-pub.key"
-    SERVER_CONFIG_PATH="$MAIN_DIRECTORY_PATH/$WG_INTERFACE.conf"
+    SERVER_CONFIG_PATH="$MAIN_DIRECTORY_PATH/{{ wg_interface }}.conf"
 }
 
 function pre_input_checks() {
@@ -158,8 +139,8 @@ function configure_directories() {
 }
 
 function configure_new_octet() {
-    OCTET_COUNT=$(($(cat $MAIN_DIRECTORY_PATH/octet.count) + 1))
-    echo $OCTET_COUNT >$MAIN_DIRECTORY_PATH/octet.count
+    OCTET_COUNT=$(($(cat ${MAIN_DIRECTORY_PATH}/octet.count) + 1))
+    echo $OCTET_COUNT >${MAIN_DIRECTORY_PATH}/octet.count
 }
 
 function generate_client_secrets() {
@@ -169,10 +150,16 @@ function generate_client_secrets() {
 }
 
 function generate_client_config() {
-    CLIENT_ALLOWED_IPV4_POOL=$(cat ./selected_ipv4_addresses) && CLIENT_ALLOWED_IPV6_POOL=$(cat ./selected_ipv6_addresses)
-    [[ $EXTERNAL_ACCESS == "true" ]] && CLIENT_ALLOWED_IPV4_POOL="0.0.0.0/0" && CLIENT_ALLOWED_IPV6_POOL="::0"
-    [[ $INTERNAL_DNS == "true" ]] && CLIENT_DNS_IPV4="${WG_SHORT_IPV4_POOL_PART}.1" && CLIENT_DNS_IPV6="${WG_SHORT_IPV6_POOL_PART}1" ||
-        CLIENT_DNS_IPV4="1.1.1.2,9.9.9.9" && CLIENT_DNS_IPV6="2606:4700:4700::1112,2620:fe::fe"
+    CLIENT_ALLOWED_IPV4_POOL=$(cat ${MAIN_DIRECTORY_PATH}/allowed_ipv4_pool.txt) && CLIENT_ALLOWED_IPV6_POOL=$(cat ${MAIN_DIRECTORY_PATH}/allowed_ipv6_pool.txt)
+    [[ $EXTERNAL_ACCESS == "true" ]] && CLIENT_ALLOWED_IPV4_POOL="0.0.0.0/0" && CLIENT_ALLOWED_IPV6_POOL="::/0"
+    if [[ $INTERNAL_DNS == "true" ]]; then
+        CLIENT_DNS_IPV4="${WG_SHORT_IPV4_POOL_PART}.1"
+        CLIENT_DNS_IPV6="${WG_SHORT_IPV6_POOL_PART}1"
+    else
+        CLIENT_DNS_IPV4="1.1.1.2,9.9.9.9"
+        CLIENT_DNS_IPV6="2606:4700:4700::1112,2620:fe::fe"
+    fi
+
     CLIENT_CONFIG_PATH="${MAIN_DIRECTORY_PATH}/clients/${CLIENT_NAME}.conf"
 
     echo "
@@ -195,8 +182,10 @@ function generate_client_config() {
     [Peer]
     PublicKey = SERVER_PUB_KEY
     PresharedKey = $CLIENT_PSK
+    # IPv4 Endpoint
     Endpoint = ${SERVER_ENDPOINT_IPV4}:$WG_PORT
-    Endpoint = [${SERVER_ENDPOINT_IPV6}]:$WG_PORT
+    # IPv6 Endpoint (disabled by default, can be used in IPv6-only scheme)
+    # Endpoint = [${SERVER_ENDPOINT_IPV6}]:$WG_PORT 
     AllowedIPs = $CLIENT_ALLOWED_IPV4_POOL
     AllowedIPs = $CLIENT_ALLOWED_IPV6_POOL" | sed 's/^[ \t]*//' >$CLIENT_CONFIG_PATH
 
@@ -204,20 +193,23 @@ function generate_client_config() {
 }
 
 function check_permissions() {
-    WG_USERNAME=$(stat -c '%U' $MAIN_DIRECTORY_PATH/clients)
-    WG_GROUP=$(stat -c '%G' $MAIN_DIRECTORY_PATH/clients)
     chown ${WG_USERNAME}:${WG_GROUP} $CLIENT_CONFIG_PATH
     chmod 600 $CLIENT_CONFIG_PATH
 }
 
 function interface_reload() {
-    systemctl reload wg-quick@$WG_INTERFACE
+    systemctl reload wg-quick@${WG_INTERFACE}
 }
 
 function firewall() {
     if [[ $SERVER_ACCESS == "true" ]]; then
         iptables -A INPUT -s ${WG_SHORT_IPV4_POOL_PART}.${OCTET_COUNT}/32 -i $WG_INTERFACE -m comment --comment "server access from $WG_INTERFACE" -j ACCEPT
         iptables-save >/etc/iptables/rules.v4
+        print_warning "added iptables rule to access server"
+
+        ip6tables -A INPUT -s ${WG_SHORT_IPV6_POOL_PART}${OCTET_COUNT}/128 -i $WG_INTERFACE -m comment --comment "server access from $WG_INTERFACE" -j ACCEPT
+        ip6tables-save >/etc/iptables/rules.v6
+        print_warning "added ip6tables rule to access server"
     fi
 }
 
@@ -227,14 +219,14 @@ function print_config() {
     cat "$CLIENT_CONFIG_PATH" | qrencode -t ansiutf8
 }
 
-(print_success "configure variables" && configure_variables) || print_error "configure variables"
+print_success "configure variables" && configure_variables || print_error "configure variables"
 print_success "pre_input_checks" && pre_input_checks
 print_success "input" && input
-(print_success "configure directories" && configure_directories) || print_error "configure directories"
-(print_success "configure new octet" && configure_new_octet) || print_error "configure new octet"
-(print_success "generate client secrets" && generate_client_secrets) || print_error "generate client secrets"
-(print_success "generate client config" && generate_client_config) || print_error "generate client config"
-(print_success "check permissions" && check_permissions) || print_error "check permissions"
-(print_success "interface reload" && interface_reload) || print_error "interface reload"
-(print_success "firewall" && firewall) || print_error "firewall"
-(print_success "print config" && print_config) || print_error "print config"
+print_success "configure directories" && configure_directories || print_error "configure directories"
+print_success "configure new octet" && configure_new_octet || print_error "configure new octet"
+print_success "generate client secrets" && generate_client_secrets || print_error "generate client secrets"
+print_success "generate client config" && generate_client_config || print_error "generate client config"
+print_success "check permissions" && check_permissions || print_error "check permissions"
+print_success "interface reload" && interface_reload || print_error "interface reload"
+print_success "firewall" && firewall || print_error "firewall"
+print_success "print config" && print_config || print_error "print config"
